8d74ed6b02c4558d034d7a067e283f27e16c5bf4
sourabh parihar <sourabh.parihar@praxontech.com>
2023-06-23 17:39:36 +0530

    login

--- seo_audit_report.py
+++ seo_audit_report.py
@@ -7,11 +7,24 @@ import time
 
 
 driver = webdriver.Chrome()
-driver.get("https://neilpatel.com/ubersuggest/")
+#driver.get("https://neilpatel.com/ubersuggest/")
+driver.get("https://app.neilpatel.com/en/login")
 
 wait= WebDriverWait(driver,20)
-ubersuggetion_box = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="btn-close"]')))
-ubersuggetion_box.click()
 
+#cookie_box = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cookiescript_accept"]')))
+#cookie_box.click()
+
+#ubersuggetion_box = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="btn-close"]')))
+#ubersuggetion_box.click()
+
+email = driver.find_element(By.XPATH, '//*[@name="email"]')
+email.send_keys("sheetal.gehlot@praxontech.com")
+
+pwd = driver.find_element(By.XPATH, '//*[@name="password"]')
+pwd.send_keys("password")
+
+login = driver.find_element(By.XPATH, '//*[@class="sc-laZMyp htFlpD"]')
+login.click()
 
 time.sleep(10)
